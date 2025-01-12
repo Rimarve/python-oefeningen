36323c13c66279a4b4f115ff8d83080cab9495b9
Rimarve <rrwvanemmerik@gmail.com>
2025-01-12 20:35:11 +0300

    Eerste commit

---
+++ calc.py
@@ -0,0 +1,5 @@
+eerste = float(input("Mijn eerste getal: "))
+tweede = float(input("Het tweede getal: "))
+
+som = eerste + tweede
+print(som)
---
+++ hello_world.py
@@ -0,0 +1,13 @@
+age = 4
+if age > 18:
+	print("Je bent volwassen.")
+else:
+	print("Je bent minderjarig.")
+
+for i in range(5):
+	print(f"Dit is loop nummer {i}")
+
+def groet(name):
+	return f"Hoi {name}!"
+
+print(groet("Rimar"))
