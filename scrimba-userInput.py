5c8b25d3c5a2966f70dc5428ad0b94facbc12feb
Rimarve <rrwvanemmerik@gmail.com>
2025-01-14 17:37:20 +0300

    User input rekenmachine leeftijd en naam

---
+++ scrimba-userInput.py
@@ -0,0 +1,10 @@
+# naam = input("Wat is mijn naam?: ")
+# leeftijd = input("Wat is mijn leeftijd?: ")
+
+# print("Hoi " + naam + "! Jij bent " + leeftijd + " jaar oud!")
+
+
+num1 = input("Eerste getal: ")
+num2 = input("Tweede getal: ")
+antwoord = float(num1) + float(num2)
+print(antwoord)
