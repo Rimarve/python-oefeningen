ec4973978aa0ce7e7710c65195eb6a987e6ab0e8
Rimarve <rrwvanemmerik@gmail.com>
2025-01-15 19:34:35 +0300

    Bewerken van lijsten

--- scrimba-tutorial.py
+++ scrimba-tutorial.py
@@ -1,5 +1,30 @@
-naam = "Rimar"
-leeftijd = 31
+sales_w1 = [7,3,42,19,15,35,9]
+sales_w2 = [12,4,26,10,7,28]
+sales = []
 
-print(f"Mijn naam is " + naam)
-print(f"Mijn leeftijd is " + str(leeftijd))
+limonade = float(input("Hoeveel limonade verkocht?"))
+
+print(limonade)
+
+
+# naam = "Rimar"
+# leeftijd = 31
+
+# print(f"Mijn naam is " + naam)
+# print(f"Mijn leeftijd is " + str(leeftijd))
+
+# vrienden = ["Niels", "Sascha", "Casper", "Rik"]
+# leeftijden = [31, 32, 22, 33]
+
+# print(vrienden[1],vrienden[2:4])
+# print(len(vrienden))
+# vrienden.reverse()
+# vrienden.sort(reverse=True)
+# print(min(vrienden))
+
+# leeftijden.sort()
+# print(max(leeftijden))
+# del vrienden[2]
+
+# print(vrienden)
+
