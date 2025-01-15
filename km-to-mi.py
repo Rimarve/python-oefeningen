2d8b609817c0c5f8c37832b23d117c88f63d7f7c
Rimarve <rrwvanemmerik@gmail.com>
2025-01-15 19:14:25 +0300

    Oefening: fstring print format, kilometers omrekenen

---
+++ km-to-mi.py
@@ -0,0 +1,6 @@
+
+naam = input("Wat is je naam?: ")
+kilometer = float(input("Aantal kilometer: "))
+miles = kilometer * 1.609
+
+print(f"Hallo {naam.capitalize()}, omgerekend is {kilometer} kilometer, {miles} miles.")
