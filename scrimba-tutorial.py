ffe67cb472a86b9b67e01bdaba8df0637f4bc4b9
Rimarve <rrwvanemmerik@gmail.com>
2025-01-15 20:25:57 +0300

    Uit de vorige gestolen if statement wat geleerd en toegepast :)

--- scrimba-tutorial.py
+++ scrimba-tutorial.py
@@ -1,11 +1,29 @@
 sales_w1 = [7,3,42,19,15,35,9]
 sales_w2 = [12,4,26,10,7,28]
-sales = []
 
-limonade = float(input("Hoeveel limonade verkocht?"))
 
-print(limonade)
+limonade = int(input("Hoeveel limonade verkocht op zondag? "))
 
+sales_w2.append(limonade)
+
+# print(limonade) // output hetgene wat je input
+# print(sales_w2) // output 2 lists 
+
+sales_total = sales_w1 + sales_w2
+print(sales_total)
+
+best_day = max(sales_total)
+worst_day = min(sales_total)
+
+stuks = "stuk" if worst_day == 1 else "stuks"
+
+
+totaal_verdiend = sum(sales_total) * 1.50
+
+print(f"Er zijn in totaal {sum(sales_total)} limonades verkocht. De beste dag was {best_day} stuks en de minste dag was {worst_day} {stuks}.")
+print(f"Je hebt in totaal € {totaal_verdiend} verdiend.")
+# print(len(sales_w1))
+# print(len(sales_w2))
 
 # naam = "Rimar"
 # leeftijd = 31
