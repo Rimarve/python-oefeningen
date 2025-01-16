11f49e89518e1fdd9db1f40ade866441e441220e
Rimarve <rrwvanemmerik@gmail.com>
2025-01-16 13:05:47 +0300

    Verder gewerkt aan lijst

--- listJoin.py
+++ listJoin.py
@@ -1,4 +1,10 @@
+# From the list above fill a list(friends_list) properly
+# with the names of all the friends. One per "slot"
+# you may need to run same command several times
+# use print() statements to work your way through the exercise
+
 csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
 friends_list = ['Exercise: fill me with names']
-print(friends_list)
+# print(friends_list)
 
+print(csv.split())
