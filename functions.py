c814ef5990af365e259923d73f620579248bb66e
Rimarve <rrwvanemmerik@gmail.com>
2025-01-18 21:37:35 +0300

    Functions functions functions

--- functions.py
+++ functions.py
@@ -1,4 +1,16 @@
-def groet(naam):
-	print(f"Hallo {naam}")
+def greeting(name, age=0):
+    #Greets user with 'name' from 'input box' and 'age', if available, default age is used
+    print(f'Hello {name.capitalize()}, you will be {age} next birthday!')
+    # print(f'We hear you like the color {color.lower()}')
 
-groet("Jan")
+# name = input('Enter your name: ')
+# age = input('Enter your age: ')
+# color = input('Enter color: ').lower()
+
+greeting("Rimar", 18)
+
+
+def value_added_tax(amount):
+	tax = amount * 0.37
+	return tax
+print(value_added_tax(100))
