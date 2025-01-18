a2b38c0fec38906a55526203ed8aab9d16bf4c23
Rimarve <rrwvanemmerik@gmail.com>
2025-01-18 19:58:22 +0300

    Prima oefening Tuples

--- tuples.py
+++ tuples.py
@@ -1,4 +1,8 @@
-friends = ['John','Michael','Terry','Eric','Graham']
-friends_tuple = ('John','Michael','Terry','Eric','Graham')
-print(friends[2:4])
-print(friends_tuple[2:4])
+friends = {'John','Michael','Terry','Eric','Graham'}
+my_friends = {'Reg','Loretta','Colin','John','Graham'}
+cars =['900','420','V70','911','996','V90','911','911','S','328','900']
+
+print("Eric" in friends and "John" in friends)
+
+set3 = friends | my_friends
+print(set3)
