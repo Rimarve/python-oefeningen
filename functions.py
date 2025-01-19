4882cc04055304990cbc1981200a666c058a656b
Rimarve <rrwvanemmerik@gmail.com>
2025-01-19 19:28:03 +0300

    Calculator

--- functions.py
+++ functions.py
@@ -1,26 +1,6 @@
-def greeting(name, age=0):
-    #Greets user with 'name' from 'input box' and 'age', if available, default age is used
-    print(f'Hello {name.capitalize()}, you will be {age} next birthday!')
-    # print(f'We hear you like the color {color.lower()}')
+print('if elif else - Exercise')
+# Create a calculator which handles +,-,*,/ and outputs answer based on the mode/ operator used
+# Hint: use 3 separate inputs 
+# Bonus: Extend functionality with extra mode so it also does celsius to fahrenheit conversion
+# formula is: temp in C*9/5 + 32 = temp in f
 
-# name = input('Enter your name: ')
-# age = input('Enter your age: ')
-# color = input('Enter color: ').lower()
-
-greeting("Rimar", 18)
-
-
-def value_added_tax(amount):
-	tax = amount * 0.37
-	total_amount = amount * 1.25
-	return f"{amount, tax, total_amount}"
-
-prijs = value_added_tax(100)
-
-print(prijs)
-
-print()
-
-a = 10
-b = 25
-print(a == b)
