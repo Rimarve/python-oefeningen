b8215a8e7467e3383d05dc4e8e7e70fc3caf372a
Rimarve <rrwvanemmerik@gmail.com>
2025-01-20 20:43:25 +0300

    python oefeningen functies met W3 resource

--- functions.py
+++ functions.py
@@ -1,18 +1,29 @@
 
+#find maximum of three numbers
+def max_of_twee(x, y):
 
-def bereken_gemiddelde(leeftijd):
+	if x > y:
+		return x
+	return y
 
-	if not leeftijd:
-		print("lege lijst")
-		return None
-	totaal = sum(leeftijd)
-	aantal = len(leeftijd)
-	gemiddelde = totaal / aantal
-	print(f"📊 Het gemiddelde van de leeftijden is: {gemiddelde:.2f} jaar")
-	return gemiddelde
+print(max_of_twee(10,15))
 
-leeftijden = [25, -30, 22, 40, 35, 28, 27, 31, 29, 33]
+
+
+
+# def bereken_gemiddelde(leeftijd):
+
+# 	if not leeftijd:
+# 		print("lege lijst")
+# 		return None
+# 	totaal = sum(leeftijd)
+# 	aantal = len(leeftijd)
+# 	gemiddelde = totaal / aantal
+# 	print(f"📊 Het gemiddelde van de leeftijden is: {gemiddelde:.2f} jaar")
+# 	return gemiddelde
+
+# leeftijden = [25, -30, 22, 40, 35, 28, 27, 31, 29, 33]
+# # bereken_gemiddelde(leeftijden)
+
 # bereken_gemiddelde(leeftijden)
-
-bereken_gemiddelde(leeftijden)
 
